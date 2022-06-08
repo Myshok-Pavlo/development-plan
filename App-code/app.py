5790c188403c7c0e06271b4c4fcda909d7d9d8b3
Myshok-Pavlo <73400841+Myshok-Pavlo@users.noreply.github.com>
2022-06-08 14:34:12 +0300

    new coomit to prom metrics

--- App-code/app.py
+++ App-code/app.py
@@ -1,19 +1,24 @@
 
   
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 app = FastAPI()
 counter =1
-
+output = '''# HELP http_requests_total The total number of HTTP requests.
+# TYPE http_requests_total counter
+'''
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/metrics")
+@app.get("/metrics", response_class=PlainTextResponse)
 def read_root():
     global counter
+    global output
     counter = counter + 1
-    return {"replies_counter " : counter}
+    return output + "replies_counter: " + str(counter)
 
 @app.get("/healthchek")
 def read_root():
